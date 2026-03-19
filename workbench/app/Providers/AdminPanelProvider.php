1715012618ca053825e5875074974c1ecd7c807e
Jordy Schreuders <3071062+99linesofcode@users.noreply.github.com>
2026-03-19 12:31:01 +0100

    feat: orchestra/workbench wired for Vite.js HMR with proper ignores
    Included a https://www.penguinui.com/ theme but I might end up using
    Filament to build custom components for the front end with as well. To
    be determined..

---
+++ workbench/app/Providers/AdminPanelProvider.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Workbench\App\Providers;
+
+use Filament\Http\Middleware\Authenticate;
+use Filament\Http\Middleware\AuthenticateSession;
+use Filament\Http\Middleware\DisableBladeIconComponents;
+use Filament\Http\Middleware\DispatchServingFilamentEvent;
+use Filament\Pages\Dashboard;
+use Filament\Panel;
+use Filament\PanelProvider;
+use Filament\Widgets\AccountWidget;
+use Filament\Widgets\FilamentInfoWidget;
+use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
+use Illuminate\Cookie\Middleware\EncryptCookies;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
+use Illuminate\Routing\Middleware\SubstituteBindings;
+use Illuminate\Session\Middleware\StartSession;
+use Illuminate\View\Middleware\ShareErrorsFromSession;
+
+class AdminPanelProvider extends PanelProvider
+{
+    public function panel(Panel $panel): Panel
+    {
+        return $panel
+            ->default()
+            ->id('admin')
+            ->path('admin')
+            ->login()
+            // ->plugin()
+            ->pages([
+                Dashboard::class,
+            ])
+            ->widgets([
+                accountwidget::class,
+                filamentinfowidget::class,
+            ])
+            ->middleware([
+                encryptcookies::class,
+                addqueuedcookiestoresponse::class,
+                startsession::class,
+                authenticatesession::class,
+                shareerrorsfromsession::class,
+                verifycsrftoken::class,
+                substitutebindings::class,
+                disablebladeiconcomponents::class,
+                dispatchservingfilamentevent::class,
+            ])
+            ->authmiddleware([
+                authenticate::class,
+            ]);
+    }
+}
